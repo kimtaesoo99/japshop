9e0ed910e0bd6ff810a3c6c06fe38e508e131c75
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-03 19:09:05 +0900

    feat: (Mebmer) 멤버 도메인 추가

---
+++ src/main/java/japbook/japshop/domain/Member.java
@@ -0,0 +1,28 @@
+package japbook.japshop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Getter
+@Setter
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+
+}
