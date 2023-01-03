42428c7d5d89a596599e15479a8003e704bf4d89
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-03 17:12:41 +0900

    Initial commit

---
+++ src/main/java/japbook/japshop/Hello.java
@@ -0,0 +1,12 @@
+package japbook.japshop;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Hello {
+
+    private String data;
+}
---
+++ src/main/java/japbook/japshop/StudyJpaApplication.java
@@ -0,0 +1,14 @@
+package japbook.japshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyJpaApplication.class, args);
+
+    }
+
+}
