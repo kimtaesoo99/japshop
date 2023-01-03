7ff2faa3d7c506957958d80e37f5b035c3a3bdea
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-03 19:06:49 +0900

    feat: (Address) 주소 도메인 추가

---
+++ src/main/java/japbook/japshop/domain/Address.java
@@ -0,0 +1,14 @@
+package japbook.japshop.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+}
