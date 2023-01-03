8eae532473e20741537f0b712146df1712de791a
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-03 20:40:20 +0900

    feat: (Category) 카테고리 도메인 추가

---
+++ src/main/java/japbook/japshop/domain/Category.java
@@ -0,0 +1,40 @@
+package japbook.japshop.domain;
+
+import japbook.japshop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    @ManyToMany // 실무에서 사용 x, 학습용
+    @JoinTable(name = "category_item",
+        joinColumns = @JoinColumn(name = "category_id"),
+        inverseJoinColumns = @JoinColumn(name = "item_id"))
+    private List<Item> items = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
+    //==연관관계 메서드==//
+    public void addChildCategory(Category child){
+        this.child.add(child);
+        child.setParent(this);
+    }
+}
