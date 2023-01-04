1ee4c233b67761a7869d37b92d79e226f7bd9dc8
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-04 17:32:29 +0900

    feat: (OrderService) 오더 서비스 추가

---
+++ src/main/java/japbook/japshop/service/OrderService.java
@@ -0,0 +1,60 @@
+package japbook.japshop.service;
+
+import japbook.japshop.domain.Delivery;
+import japbook.japshop.domain.Member;
+import japbook.japshop.domain.Order;
+import japbook.japshop.domain.OrderItem;
+import japbook.japshop.domain.item.Item;
+import japbook.japshop.repository.ItemRepository;
+import japbook.japshop.repository.MemberRepository;
+import japbook.japshop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+
+    //주문
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count){
+        //엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        //배송정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        //주문 상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        //주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        //주문 저장
+        orderRepository.save(order);
+        return order.getId();
+    }
+
+    //취소
+    @Transactional
+    public void cancelOrder(Long orderId){
+        //주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+        //주문 취소
+        order.cancel();
+    }
+
+    //검색
+//    public List<Order> findOrders(OrderSearch orderSearch){
+//        return orderRepository.findAll(orderSearch);
+//    }
+}
