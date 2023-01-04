12fa58b145989bf99e728bb865805942149f19af
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-04 17:31:15 +0900

    feat: (OrderItem) 비즈니스 로직 추I

--- src/main/java/japbook/japshop/domain/OrderItem.java
+++ src/main/java/japbook/japshop/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package japbook.japshop.domain;
 
 import japbook.japshop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -9,6 +11,7 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
 
@@ -28,4 +31,27 @@ public class OrderItem {
     private int orderPrice;
 
     private int count;
+
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+
+    //==비즈니스 로직==//
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
