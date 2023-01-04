5593e267c6fdc0339fed183e479eb78bdf22f31e
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-04 17:32:10 +0900

    feat: (OrderSearch) 오더 검색 추가

---
+++ src/main/java/japbook/japshop/repository/OrderSearch.java
@@ -0,0 +1,14 @@
+package japbook.japshop.repository;
+
+
+import japbook.japshop.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderSearch {
+
+    private String memberName; //회원이름
+    private OrderStatus orderStatus; //ORDER,CANCEL
+}
