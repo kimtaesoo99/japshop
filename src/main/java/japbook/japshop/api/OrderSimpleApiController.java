47be7144bd7d57b62bac29fc4fc3ea453dead6fc
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-09 15:51:14 +0900

    feat: (OrderSimpleApiController) 주문 조회 version2 추가

--- src/main/java/japbook/japshop/api/OrderSimpleApiController.java
+++ src/main/java/japbook/japshop/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package japbook.japshop.api;
 
+import japbook.japshop.domain.Address;
 import japbook.japshop.domain.Order;
+import japbook.japshop.domain.OrderStatus;
 import japbook.japshop.repository.OrderRepository;
 import japbook.japshop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -16,7 +21,7 @@ public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
 
-    // 무한 참조일어남 -> @JsonIgnore하면 프록시객체나옴
+    // 무한 참조일어남 -> @JsonIgnore하면 프록시객체나가
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1(){
         List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
@@ -25,6 +30,32 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress(); //Lazy 강제 초기화
         }
         return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2(){
+        //Order 2개
+        // N + 1 -> 1 + 회원 N(2) + 배송 N(2)
+        List<Order> orders = orderRepository.findAllByCriteria(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream().map(o -> new SimpleOrderDto(o)).collect(Collectors.toList());
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();  //Lazy 초기화
+            orderDate = order.getLocalDateTime();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();  //Lazy 초기화
+        }
     }
 
 
