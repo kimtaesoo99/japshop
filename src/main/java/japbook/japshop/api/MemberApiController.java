5ea52bf6e23f513ea7d4a98ea62e5659c2990530
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-09 13:28:52 +0900

    feat: (MemberApiController) MemberApiController Version2 추가

--- src/main/java/japbook/japshop/api/MemberApiController.java
+++ src/main/java/japbook/japshop/api/MemberApiController.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -20,6 +21,19 @@ public class MemberApiController {
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
+    }
+
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request){
+        Member member =new Member();
+        member.setName(request.name);
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberRequest{
+        private String name;
     }
 
     @Data
