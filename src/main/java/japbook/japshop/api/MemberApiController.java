86672bcffdc747b36135b10c5977a1af23d01518
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-09 13:41:20 +0900

    feat: (MemberApiController) 회원 정보 업데이트 추가

--- src/main/java/japbook/japshop/api/MemberApiController.java
+++ src/main/java/japbook/japshop/api/MemberApiController.java
@@ -2,12 +2,10 @@ package japbook.japshop.api;
 
 import japbook.japshop.domain.Member;
 import japbook.japshop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -30,6 +28,28 @@ public class MemberApiController {
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMembmerResponse updateMemberV2(@PathVariable("id") Long id,
+                                                @RequestBody @Valid UpdateMembmerRequest request){
+
+        memberService.update(id,request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMembmerResponse(findMember.getId(), findMember.getName());
+    }
+
+    @Data
+    static class UpdateMembmerRequest{
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMembmerResponse{
+        private Long id;
+        private String name;
+    }
+
 
     @Data
     static class CreateMemberRequest{
