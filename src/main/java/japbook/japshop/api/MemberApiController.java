0bc57092dba92795dd55001dcad0ce84acc96ee0
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-09 13:58:31 +0900

    feat: (MemberController) 조회 Version2 추가

--- src/main/java/japbook/japshop/api/MemberApiController.java
+++ src/main/java/japbook/japshop/api/MemberApiController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController //@Controller + @ResponseBody
 @RequiredArgsConstructor
@@ -20,6 +21,27 @@ public class MemberApiController {
     public List<Member> membersV1(){
         return memberService.findMembers();
     }
+
+    @GetMapping("/api/v2/members")
+    public Result membersV2(){
+        List<MemberDto> collect = memberService.findMembers().stream()
+            .map(m -> new MemberDto(m.getName())).collect(Collectors.toList());
+
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T>{
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto{
+        private String name;
+    }
+
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
