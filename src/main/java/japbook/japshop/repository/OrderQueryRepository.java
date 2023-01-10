fddbf2c2a2dc77810c65ea81406cff474fbd5c47
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-10 19:29:02 +0900

    feat: (OrderQueryRepository) 주문상품 조회 (select2번에 해결하는 메서드)추가

--- src/main/java/japbook/japshop/repository/OrderQueryRepository.java
+++ src/main/java/japbook/japshop/repository/OrderQueryRepository.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -18,6 +20,32 @@ public class OrderQueryRepository {
             o.setOrderItems(orderItems);
         });
         return result;
+    }
+
+    public List<OrderQueryDto> findAllByDto_optimization() {
+        List<OrderQueryDto> result = findOrders();
+
+        List<Long> orderIds = toOrderIds(result);
+
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = findOrderItemMap(orderIds);
+
+        result.forEach(o ->o.setOrderItems(orderItemMap.get(o.getOrderId())));
+        return result;
+    }
+
+    private Map<Long, List<OrderItemQueryDto>> findOrderItemMap(List<Long> orderIds) {
+        List<OrderItemQueryDto> orderItems = em.createQuery("select new japbook.japshop.repository.OrderItemQueryDto(oi.order.id, i.name,oi.orderPrice,oi.count) " +
+                "from OrderItem oi join oi.item i where  oi.order.id in :orderIds", OrderItemQueryDto.class)
+            .setParameter("orderIds", orderIds).getResultList();
+
+        Map<Long, List<OrderItemQueryDto>> orderItemMap
+            = orderItems.stream().collect(Collectors.groupingBy(orderItemQueryDto -> orderItemQueryDto.getOrderId()));
+        return orderItemMap;
+    }
+
+    private static List<Long> toOrderIds(List<OrderQueryDto> result) {
+        List<Long> orderIds = result.stream().map(OrderQueryDto::getOrderId).collect(Collectors.toList());
+        return orderIds;
     }
 
     private List<OrderItemQueryDto> findOrderItems(Long orderId) {
