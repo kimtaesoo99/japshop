3eab2c3362ace19999015d12d4a36faaa1a02b57
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-10 20:13:45 +0900

    feat: (OrderQueryDto) 주문 아이템도 받는 생성자 추가

--- src/main/java/japbook/japshop/repository/OrderQueryDto.java
+++ src/main/java/japbook/japshop/repository/OrderQueryDto.java
@@ -3,11 +3,13 @@ package japbook.japshop.repository;
 import japbook.japshop.domain.Address;
 import japbook.japshop.domain.OrderStatus;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@EqualsAndHashCode(of = "orderId")
 public class OrderQueryDto {
 
     private Long orderId;
@@ -23,5 +25,14 @@ public class OrderQueryDto {
         this.orderDate = orderDate;
         this.orderStatus = orderStatus;
         this.address = address;
+    }
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, List<OrderItemQueryDto> orderItems) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.orderItems = orderItems;
     }
 }
